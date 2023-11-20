1c01e97ec6c4fd8ef68933a18ac519b65a54f4d4
davidrnmdr <davidrnmdr@gmail.com>
2023-11-20 15:40:12 -0300

    feat: saves user email at FaunaDB when sign in

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+import { fauna } from "../../../services/fauna";
+import { query } from "faunadb";
+
 export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -14,6 +18,38 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        console.log(user.email);
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index("user_by_email"),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(query.Collection("users"), {
+              data: { email: user.email },
+            }),
+            query.Get(
+              query.Match(
+                query.Index("user_by_email"),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 };
 
 export default NextAuth(authOptions);
