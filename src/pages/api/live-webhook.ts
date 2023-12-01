191a426962e5df43134d31666f01eb4dc0a5c40b
davidrnmdr <davidrnmdr@gmail.com>
2023-12-01 12:23:48 -0300

    creation: live-webhook

---
+++ src/pages/api/live-webhook.ts
@@ -0,0 +1,83 @@
+import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
+
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk == "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const relevantEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.deleted",
+  "customer.subscription.updated",
+]);
+
+app.post("/api/express-webhook", async (req, res) => {
+  const buf = await buffer(req);
+  const secret = req.headers["stripe-signature"];
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      buf,
+      secret,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (e) {
+    res.status(400).send(`Webhook error: ${e.message}`);
+    return;
+  }
+
+  const { type } = event;
+
+  if (relevantEvents.has(type)) {
+    try {
+      switch (type) {
+        case "customer.subscription.updated":
+        case "customer.subscription.deleted":
+          const subscription = event.data.object as Stripe.Subscription;
+
+          await saveSubscription(
+            subscription.id,
+            subscription.customer.toString(),
+            false
+          );
+
+          break;
+
+        case "checkout.session.completed":
+          const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+          saveSubscription(
+            checkoutSession.subscription.toString(),
+            checkoutSession.customer.toString(),
+            true
+          );
+          break;
+        default:
+          throw new Error("Unhandled event.");
+      }
+    } catch (e) {
+      return res.json({ error: "Webhook failed to handle event." });
+    }
+  }
+
+  res.status(200).json({ ok: true });
+});
