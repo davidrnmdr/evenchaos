12c5c5dba663e834fecad59aafae1be79f6fb362
davidrnmdr <davidrnmdr@gmail.com>
2023-12-01 12:54:04 -0300

    fix: live-webhook

--- src/pages/api/live-webhook.ts
+++ src/pages/api/live-webhook.ts
@@ -6,6 +6,12 @@ import bodyParser from "body-parser";
 
 import express from "express";
 
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -15,49 +21,54 @@ const relevantEvents = new Set([
   "customer.subscription.updated",
 ]);
 
-app.post("/api/live-webhook", async (req, res) => {
-  let event: Stripe.Event;
+app.post(
+  "/api/live-webhook",
+  bodyParser.raw({ type: "application/json" }),
+  async (req, res) => {
+    let event: Stripe.Event;
 
-  try {
-    event = req.body;
-  } catch (e) {
-    res.status(400).send(`Webhook error: ${e.message}`);
-    return;
-  }
-
-  const { type } = event;
-
-  if (relevantEvents.has(type)) {
     try {
-      switch (type) {
-        case "customer.subscription.updated":
-        case "customer.subscription.deleted":
-          const subscription = event.data.object as Stripe.Subscription;
-
-          await saveSubscription(
-            subscription.id,
-            subscription.customer.toString(),
-            false
-          );
-
-          break;
-
-        case "checkout.session.completed":
-          const checkoutSession = event.data.object as Stripe.Checkout.Session;
-
-          saveSubscription(
-            checkoutSession.subscription.toString(),
-            checkoutSession.customer.toString(),
-            true
-          );
-          break;
-        default:
-          throw new Error("Unhandled event.");
-      }
+      event = req.body;
     } catch (e) {
-      return res.json({ error: "Webhook failed to handle event." });
+      res.status(400).send(`Webhook error: ${e.message}`);
+      return;
     }
-  }
 
-  res.status(200).json({ ok: true });
-});
+    const type = event.type;
+
+    if (relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+
+            break;
+
+          case "checkout.session.completed":
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
+
+            saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            );
+            break;
+          default:
+            throw new Error("Unhandled event.");
+        }
+      } catch (e) {
+        return res.json({ error: "Webhook failed to handle event." });
+      }
+    }
+
+    res.status(200).json({ ok: true });
+  }
+);
